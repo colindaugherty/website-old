028e0f4b16f5201a8d7388e1eb7061e766ee94cb
Colin Daugherty <colinddaugherty@gmail.com>
2019-01-25 19:08:00 -0500

    updating cron job

--- cron_pull.sh
+++ cron_pull.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd /home/pi/Desktop/Development/website
 git pull origin master
+sudo a2dissite colindaugherty.conf
+sudo /etc/init.d/apache2 reload
+sudo a2ensite colindaugherty.conf
+sudo /etc/init.d/apache2 reload
