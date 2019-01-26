d6074346ab756d48e5d9ca630a9e8aa20bfb1b06
Colin Daugherty <colinddaugherty@gmail.com>
2019-01-25 20:01:56 -0500

    permission issues? i think

--- cron_pull.sh
+++ cron_pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/pi/Desktop/Development/website
-git pull origin master
+sudo git pull origin master
 sudo a2dissite colindaugherty.conf
 sudo /etc/init.d/apache2 reload
 sudo a2ensite colindaugherty.conf
