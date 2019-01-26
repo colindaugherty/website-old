188b9bb4b4af1a0507bdb80ba9f979441cca38f0
Colin Daugherty <colinddaugherty@gmail.com>
2019-01-25 19:05:36 -0500

    forgot to setup functions for future pages

--- server.py
+++ server.py
@@ -6,5 +6,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/about")
+def about():
+    return render_template('coming_soon.html')
+
+@app.route("/about")
+def projects():
+    return render_template('coming_soon.html')
+
+@app.route("/about")
+def portfolio():
+    return render_template('coming_soon.html')
+
+@app.route("/about")
+def blog():
+    return render_template('coming_soon.html')
+
+@app.route("/about")
+def contact():
+    return render_template('coming_soon.html')
+
+
 if __name__ == "__main__":
     app.run()
