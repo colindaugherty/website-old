6e7d34d89cb193e106816917538e796f874126e5
Colin Daugherty <colinddaugherty@gmail.com>
2019-01-29 14:51:30 -0500

    created a 500 page, tweaked the look of error pages, started index

--- server.py
+++ server.py
@@ -34,5 +34,9 @@ def social():
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def server_overloaded(e):
+    return render_template('500.html'), 500
+
 if __name__ == "__main__":
     app.run()
